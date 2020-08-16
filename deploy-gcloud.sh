3221fe4dbdae3e47a0e230d40568734f64327d1d
Amit Jain <amitjain628@gmail.com>
2020-08-16 14:21:18 +0530

    Del files

--- deploy-gcloud.sh
+++
@@ -1,2 +0,0 @@
-gcloud builds submit --tag gcr.io/hackernews-275418/hackernews
-gcloud run deploy --image gcr.io/hackernews-275418/helloworld --platform managed
--- deploy-local.sh
+++
@@ -1,2 +0,0 @@
-docker build -t hackernews .
-docker run -it --rm -p 8080:3000 hackernews
